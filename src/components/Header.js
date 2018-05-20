f801f1201195ed92790a3c53ba9bbf30add07994
ninjasun <davide.mezzetti@hotmail.it>
2018-05-20 14:38:02 +0200

    style hamburger and logo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import MaterialIcon from 'material-icons-react';
-import logoPath from '../youtube_logo.png'
+import logoPath from '../youtube_logo.png';
+
+
 class Header extends React.Component{
 
     render(){
         return(
             <header className='header'>
-
+                <div className="header-left">
                 <button className="hamburger-menu">
                     <MaterialIcon icon="menu" size="small"/>
                 </button>
@@ -14,14 +16,16 @@ class Header extends React.Component{
                 <div className="logo-container">
                     <img className="logo-img" src={logoPath} alt="youtube logo" />
                 </div>
-
-                <div className="search-bar">
-                    <input className="search-input"/>
-                    <button className="search-button">
-                        <MaterialIcon icon="search" size="small"/>
-                    </button>
                 </div>
-
+                <div className="header-center">
+                    <div className="search-bar">
+                        <input className="search-input"/>
+                        <button className="search-button">
+                            <MaterialIcon icon="search" size="small"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="header-right">
                 <button className="new-post-button">
                     <MaterialIcon icon="video_call" size="small"/>
                 </button>
@@ -31,13 +35,13 @@ class Header extends React.Component{
                 </button>
 
                 <button className="notification-button">
-                    <MaterialIcon icon="notification" size="small"/>
+                    <MaterialIcon icon="notifications" size="small"/>
                 </button>
 
                 <button className="account-circle">
                     <MaterialIcon icon="account_circle" size="small"/>
                 </button>
-
+                </div>
             </header>
         )
     }

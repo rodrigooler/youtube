6f7dd713f5309542a261ce25e19966e00778252d
ninjasun <davide.mezzetti@hotmail.it>
2018-05-21 12:10:22 +0200

    styling sidemenu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Sidemenu from './components/Sidemenu';
+import 'normalize.css';
 import './App.css';
 
 class App extends Component {
--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -2,6 +2,15 @@ import React, {Component} from 'react';
 import './sidemenu.css';
 import MaterialIcon from 'material-icons-react';
 
+import ball_icon from '../ball_icon.jpg';
+import music_icon from '../music_icon.jpg';
+import gamepad_icon from '../gamepad_icon.jpg';
+import live_icon from '../live_icon.jpg';
+import movie_icon from '../movie_icon.jpg';
+import news_icon from '../news_icon.jpg';
+import video_360_icon from '../video_360_icon.jpg';
+
+
 class Sidemenu extends Component {
     render(){
         return(
@@ -31,12 +40,12 @@ class Sidemenu extends Component {
 
                 <hr className="divider" />
 
-                <h1 className="sidemenu-title">IL MEGLIO DI YOUTUBE</h1>
+                <h3 className="sidemenu-title">IL MEGLIO DI YOUTUBE</h3>
 
                 <ul className="navigation">
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="music_note" size="small"/>
+                            <img src={music_icon} alt="Musica"  />
                             <span className="item-text"> Musica</span>
 
                         </a>
@@ -44,48 +53,48 @@ class Sidemenu extends Component {
 
                     <li className="item">
                         <a>
-                            <MaterialIcon icon="whatshot" size="small"/>
+                            <img src={ball_icon} alt="Sport"  />
                             <span className="item-text"> Sport</span>
                         </a>
                     </li>
 
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="games" size="small"/>
+                            <img src={gamepad_icon} alt="Games"  />
                             <span className="item-text"> Giochi</span>
                         </a>
                     </li>
 
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="local_movies" size="small"/>
+                            <img src={movie_icon} alt="Movie"  />
                             <span className="item-text"> Film</span>
                         </a>
                     </li>
 
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="local_movies" size="small"/>
+                            <img src={movie_icon} alt="TV"  />
                             <span className="item-text"> Programmi TV</span>
                         </a>
                     </li>
 
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="history" size="small"/>
+                            <img src={news_icon} alt="News"  />
                             <span className="item-text"> Notizie</span>
                         </a>
                     </li>
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="history" size="small"/>
+                            <img src={live_icon} alt="Live"  />
                             <span className="item-text"> Dal vivo</span>
                         </a>
                     </li>
 
                     <li className="item">
                         <a >
-                            <MaterialIcon icon="history" size="small"/>
+                            <img src={video_360_icon} alt="Video 360"  />
                             <span className="item-text"> Video a 360 gradi</span>
                         </a>
                     </li>
@@ -106,7 +115,7 @@ class Sidemenu extends Component {
                 <hr className="divider"/>
 
                 <div className="login-section">
-                    <h2>  Accedi ora per vedere i tuoi canali e i consigli.</h2>
+                    <h3>  Accedi ora per vedere i tuoi canali e i consigli.</h3>
                     <button className="login-button">ACCEDI</button>
                 </div>
 
